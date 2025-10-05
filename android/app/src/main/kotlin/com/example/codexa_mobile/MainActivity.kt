95e3ff4d35c8350c1501d71a262a63bac92855a3
abdelrhman-mostafa95 <abdelrhman.mostafa1095@gmail.com>
2025-10-05 22:13:31 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/codexa_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.codexa_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:codexa_mobile/Ui/home_page/home_page/homeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes:{
+        '/':(context) => Homescreen(),
+      },
+    );
+  }
+}
+
+
+// login
+// register
+// splash screen + on boarding
+// landing page  => bottom navbar ==> home - courses - community - settings (theme - lanaguage - payment - history) (profile-screen - notifications - search-textfeild - chat-screen)
+// 7-10-2025 ==> 11:59 pm
