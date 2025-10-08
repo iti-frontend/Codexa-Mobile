c54d37a1073b07b0a7800012aa2d26dff0fc08cf
PeterNasrGab <peter.nasr.gab@gmail.com>
2025-10-08 15:53:11 +0300

    added login and register UI

---
+++ lib/Ui/utils/widgets/custom_text_field.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final IconData? icon;
+
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        hintText: hintText,
+        prefixIcon: icon != null ? Icon(icon) : null,
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
