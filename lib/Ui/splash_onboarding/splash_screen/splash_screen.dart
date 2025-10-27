1ad7f326be0125b4fa18cd9eab82e5d9312a34b1
abdelrhman-mostafa95 <abdelrhman.mostafa1095@gmail.com>
2025-10-28 00:03:52 +0300

    fix conflict

--- lib/Ui/splash_onboarding/splash_screen/splash_screen.dart
+++ lib/Ui/splash_onboarding/splash_screen/splash_screen.dart
@@ -1,5 +1,8 @@
+import 'package:codexa_mobile/Ui/home_page/home_screen/home_screen.dart';
 import 'package:codexa_mobile/Ui/splash_onboarding/on_boarding/onboarding_screen.dart';
+import 'package:codexa_mobile/Ui/utils/provider_ui/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatefulWidget {
   static const String routeName = '/';
@@ -14,8 +17,17 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 2), () {
-      Navigator.pushReplacementNamed(context, OnboardingScreen.routeName);
+
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+
+    userProvider.loadUser().then((_) {
+      Future.delayed(const Duration(seconds: 2), () {
+        if (userProvider.token != null && userProvider.role != null) {
+          Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+        } else {
+          Navigator.pushReplacementNamed(context, OnboardingScreen.routeName);
+        }
+      });
     });
   }
 
